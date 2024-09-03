e1cc678276d32742ee1c4c20c52455b0f95ac30c
CorneliusNgure <kingcornelius07@gmail.com>
2024-09-03 02:39:36 -0400

    Simple pagination

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""Server class that defines various methods to paginate a CSV file"""
+
+from typing import Tuple, List
+import csv
+import math
+
+
+class Server:
+    """
+    Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        """
+        Constructor that's automatically called 
+        when class instance is created
+        """
+        # Initializes private instance variable to None
+        # The variable will hold the dataset/CSV file 
+        self.__dataset = None 
+
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Returns a specific page of the dataset."""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start_index, end_index = index_range(page, page_size)
+        dataset = self.dataset()
+        
+        if start_index >= len(dataset):
+            return []
+        
+        return dataset[start_index:end_index]
+
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Returns a tuple of start and end indices of a paginated data
+    
+    Args:
+        - page (int): 1-indexed page no.
+        - page_size (int): No. of rows per page.
+
+    Returns:
+        - tuple containing start and end indices of paginated data.
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size 
+    return (start_index, end_index)
