f82e78faa8ea1c25b71b565c76a24c7fb5270d75
CorneliusNgure <kingcornelius07@gmail.com>
2024-09-03 02:43:29 -0400

    Simple Pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -14,13 +14,12 @@ class Server:
 
     def __init__(self):
         """
-        Constructor that's automatically called 
+        Constructor that's automatically called
         when class instance is created
         """
         # Initializes private instance variable to None
-        # The variable will hold the dataset/CSV file 
-        self.__dataset = None 
-
+        # The variable will hold the dataset/CSV file
+        self.__dataset = None
 
     def dataset(self) -> List[List]:
         """Cached dataset"""
@@ -32,7 +31,6 @@ class Server:
 
         return self.__dataset
 
-
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Returns a specific page of the dataset."""
         assert isinstance(page, int) and page > 0
@@ -40,18 +38,17 @@ class Server:
 
         start_index, end_index = index_range(page, page_size)
         dataset = self.dataset()
-        
+
         if start_index >= len(dataset):
             return []
-        
-        return dataset[start_index:end_index]
 
+        return dataset[start_index:end_index]
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Returns a tuple of start and end indices of a paginated data
-    
+
     Args:
         - page (int): 1-indexed page no.
         - page_size (int): No. of rows per page.
@@ -60,5 +57,5 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
         - tuple containing start and end indices of paginated data.
     """
     start_index = (page - 1) * page_size
-    end_index = start_index + page_size 
+    end_index = start_index + page_size
     return (start_index, end_index)
