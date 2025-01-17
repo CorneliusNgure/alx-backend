7cd7abed21dc3d5932e4a4a0815ead08608e4c6a
CorneliusNgure <kingcornelius07@gmail.com>
2025-01-16 20:41:37 -0800

    Create the Job processor

---
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,21 @@
+import kue from 'kue';
+
+// Create a queue
+const queue = kue.createQueue();
+
+// Function to send a notification
+function sendNotification(phoneNumber, message) {
+    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+}
+
+// Process the queue
+queue.process('push_notification_code', (job, done) => {
+    const { phoneNumber, message } = job.data;
+    
+    // Call sendNotification with the job data
+    sendNotification(phoneNumber, message);
+
+    // Mark the job as done
+    done();
+});
+
